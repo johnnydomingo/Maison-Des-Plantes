ec7c0b6f60d31b5ab590c1178393a749b5ae1c30
Johnny Domingo <johnnydomingo@Johnnys-MBP.lan>
2021-12-10 17:24:43 -0500

    Seed Data and Routes Complete

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,57 @@
+class PostsController < ApplicationController
+  before_action :set_post, only: :show
+  before_action :authorize_request, only: [:create, :update, :destroy]
+  before_action :set_user_food, only: [:update, :destroy]
+
+  # GET /posts
+  def index
+    @posts = Post.all
+
+    render json: @posts
+  end
+
+  # GET /posts/1
+  def show
+    render json: @post
+  end
+
+  # POST /posts
+  def create
+    @post = Post.new(post_params)
+    @post.user = current_user
+    if @post.save
+      render json: @post, status: :created
+    else
+      render json: @post.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /posts/1
+  def update
+    if @post.update(post_params)
+      render json: @post
+    else
+      render json: @post.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /posts/1
+  def destroy
+    @post.destroy
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_post
+      @post = Post.find(params[:id])
+    end
+
+    def set_user_post
+      @post = @current_user.posts.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def post_params
+      params.require(:post).permit(:content, :image_url)
+    end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Post.destroy_all
+User.destroy_all
+
+@sadie = User.create!(first_name: 'Sadie',  last_name: 'Doyle', email: 'sadie@email.com', password: '123456')
+@oliver = User.create!(first_name: 'Oliver',  last_name: 'Wylee', email: 'oliver@email.com', password: '123456')
+@tamara = User.create!(first_name: 'Tamara',  last_name: 'Key', email: 'tamara@email.com', password: '123456')
+@jake = User.create!(first_name: 'Jake',  last_name: 'Sloan', email: 'jake@email.com', password: '123456')
+@dana = User.create!(first_name: 'Dana',  last_name: 'Peterson', email: 'dana@email.com', password: '123456')
+@andie = User.create!(first_name: 'Andie', last_name: 'Wallace', email: 'andie@email.com', password: '123456')
+
+puts "#{User.count} users created"
+
+sadie1 = Post.create!(content: 'Added my new Silver Satin to the bathroom counter top. Brings everything together so nicely! Plants in the bathroom make my morning routine that much better.', image_url: 'https://cdn.shopify.com/s/files/1/0150/6262/products/the-sill_silver-satin_gallery_small_all_all_07_720x.jpg?v=1629121920', user: @sadie)
+
+oliver1 = Post.create!(content: 'I just got a new Birds of Paradise last week and I have to be honest, she is thriving. Had to put it by the best view in the new apartment. She also loves to listen to Bob Marley - I play some tracks everytime I water her.', image_url: 'https://cdn.shopify.com/s/files/1/0150/6262/products/the-sill_bird-of-paradise_gallery_05_720x.jpg?v=1624309208', user: @oliver)
+
+tamara1 = Post.create!(content: 'My snake laurentii looks perfect in this lighting! Picked it up this past weekend at the farmer’s market. It’s my first
+one though.. any tips on how to better take care of this?', image_url: 'https://cdn.shopify.com/s/files/1/0150/6262/products/the-sill_large-snake-plant-laurentii_gallery_04_720x.jpg?v=1624310218', user: @tamara)
+
+puts "#{Post.count} posts created"
