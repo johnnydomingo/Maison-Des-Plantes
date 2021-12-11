301ebbdd7857e5f830823d72d724b4682175be08
Johnny Domingo <johnnydomingo@Johnnys-MBP.lan>
2021-12-11 13:27:12 -0500

    Update Front End

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,62 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import { Switch, Route, useHistory } from 'react-router-dom';
+import Layout from './layouts/Layout';
+import Login from './screens/Login';
+import {
+  loginUser,
+  registerUser,
+  verifyUser,
+  removeToken,
+} from './services/auth';
+import Register from './services/register';
+import MainContainer from './containers/MainContainer';
 
 function App() {
+  const [currentUser, setCurrentUser] = useState(null);
+  const history = useHistory;
+
+  useEffect(() => {
+    const handleVerify = async () => {
+      const userData = await verifyUser();
+      setCurrentUser(userData);
+    };
+    handleVerify();
+  }, []);
+
+  const handleLogin = async (formData) => {
+    const userData = await loginUser(formData);
+    setCurrentUser(userData);
+    history.push('/');
+  };
+
+  const handleRegister = async (formData) => {
+    const userData = await registerUser(formData);
+    setCurrentUser(userData);
+    history.push('/');
+  };
+
+  const handleLogout = () => {
+    setCurrentUser(null);
+    localStorage.removeItem('authToken');
+    removeToken();
+  };
+
   return (
     <div className="App">
-      
+        <Layout currentUser={currentUser} handleLogout={handleLogout}>
+        <Switch>
+          <Route path='/login'>
+            <Login handleLogin={handleLogin} />
+          </Route>
+          <Route path='/create-account'>
+            <Register handleRegister={handleRegister} />
+          </Route>
+          <Route path='/'>
+            <MainContainer currentUser={currentUser}/>
+          </Route>
+        </Switch>
+      </Layout>
     </div>
   );
 }
--- client/src/containers/MainContainer.jsx
+++ client/src/containers/MainContainer.jsx
@@ -1,10 +1,16 @@
 import { useEffect, useState } from 'react';
-
+import { Switch, Route, useHistory } from 'react-router-dom';
+import PostCreate from '../screens/PostCreate';
+import PostEdit from '../screens/PostEdit';
+import Home from '../screens/Home';
+import PostDetail from '../screens/PostDetails';
+import { getAllPosts, postPost, putPost, deletePost } from '../services/post';
 
 
 
 const MainContainer = () => {
   const [posts, setPosts] = useState([]);
+  const history = useHistory();
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -14,12 +20,47 @@ const MainContainer = () => {
     fetchPosts;
   }, []);
 
+  const handlePostCreate = async (formData) => {
+    const newPost = await postPost(formData);
+    setFoods((prevState) => [...prevState, newPost]);
+    history.push('/');
+  };
 
+  const handlePostUpdate = async (id, formData) => {
+    const newPost = await putPost(id, formData);
+    setPosts((prevState) =>
+      prevState.map((post) => {
+        return post.id === Number(id) ? newPost : post;
+      })
+    );
+    history.push('/');
+  };
 
-
+  const handlePostDelete = async (id) => {
+    await deletePost(id);
+    setPosts((prevState) => prevState.filter((post) => post.id !== id));
+  };
 
   return (
     <div>
+      <Switch>
+        <Route path='/posts/:id/edit'>
+          <PostEdit posts={posts} handlePostUpdate={handlePostUpdate} />
+        </Route>
+        <Route path='/posts/new'>
+          <PostCreate handlePostCreate={handlePostCreate}/>
+        </Route>
+        <Route path='/posts/:id'>
+          <PostDetail currentUser={currentUser} />
+        </Route>
+        <Route path='/'>
+          <Home
+            posts={posts}
+            handlePostDelete={handlePostDelete}
+            currentUser={currentUser}
+          />
+        </Route>
+      </Switch>
     </div>
   )
 }
