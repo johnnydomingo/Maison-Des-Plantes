d0ace4703649c893f8295e32a4d0ad0f059fade4
Johnny Domingo <johnnydomingo@Johnnys-MBP.lan>
2021-12-11 17:06:22 -0500

    Update Nav

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -3,4 +3,20 @@ import { Link } from 'react-router-dom';
 
 
 
-const Nav 
+const Nav = ({ currentUser }) => {
+  return (
+    <div className="auth-options">
+    {currentUser ? <Link to="/">MAISON des PLANTES</Link> : null}
+    {currentUser ? <Link to="/create-post">Drop-a-Seed</Link> : null}
+    <p>{currentUser ? `Welcome, ${currentUser.firstName.lastName}` : null}</p>
+    {currentUser ? null : <Link to="/register">Register</Link>}
+    {currentUser ? (
+      <button className="signout-btn" onClick={handleLogout}>Sign Out</button>
+    ) : (
+      <Link to="/login">Login</Link>
+    )}
+  </div>
+  )
+}
+
+export default Nav;
