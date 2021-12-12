69f77e812ed5d3bb63d7bf2f0e3ea5507821b21f
Johnny Domingo <johnnydomingo@Johnnys-MBP.lan>
2021-12-12 13:51:34 -0500

    Update Post Delete

--- client/src/components/Feed.jsx
+++ client/src/components/Feed.jsx
@@ -1,19 +1,28 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Feed = ({ posts, handlePostDelete, currentUser }) => {
   // logic to connect user_id to first_name and last_name
   return (
     <div>
       {posts.map((post) => (
-        <div>
+        <div key={post.id}>
           <p>{post.user_id}</p>
           <p>{post.content}</p>
           <img src={post.image_url} />
+
+          {currentUser?.id === post.user_id && (
+            <>
+              <Link to={`/posts/${post.id}/edit`}>
+                <button>Edit</button>
+              </Link>
+              <button onCLick={() => handlePostDelete(post.id)}>Delete</button>
+            </>
+          )}
         </div>
       ))}
-    
     </div>
-  )
-}
+  );
+};
 
 export default Feed;
