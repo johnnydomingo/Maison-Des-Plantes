90e33d7b5574ea5abec1484b9bf62f3c63074537
Johnny Domingo <johnnydomingo@Johnnys-MBP.lan>
2021-12-12 11:03:24 -0500

    Update

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,21 +1,103 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 // import account from "../assets/account.png";
+
+// const authenticatedOptions = (
+//   <>
+//     {/* <NavLink className="link" to="/create-post">
+//       Drop-a-Seed
+//     </NavLink> */}
+//     <NavLink className="link" to="/sign-out">
+//       Sign Out
+//     </NavLink>
+//   </>
+// );
+// const unauthenticatedOptions = (
+//   <>
+//     {/* <NavLink className="link" to="/sign-up">
+//       Sign Up
+//     </NavLink> */}
+//     <NavLink className="link" to="/login">
+//       Login
+//     </NavLink>
+//   </>
+// );
+// const alwaysOptions = (
+//   <>
+//     <NavLink className="link" to="/create-post">
+//       Drop-a-Seed
+//     </NavLink>
+//     <NavLink className="link" to="/about">
+//       About
+//     </NavLink>
+//     <NavLink className="link" to="/how-it-works">
+//       How it Works
+//     </NavLink>
+//   </>
+// );
 const Nav = ({ currentUser, handleLogout, children }) => {
   return (
     <div>
       <header>
+        <NavLink to="/"><h1>Maison des Plantes</h1></NavLink>
+        <NavLink to="/create-post">Drop-a-Seed</NavLink>
+            <NavLink to="/how-it-works">How It Works</NavLink>
+            <NavLink to="/about">About</NavLink>
+        {currentUser ? (
+          <div>
+            <p>{currentUser.first_name}</p>
+            <button onClick={handleLogout}>Logout</button>
+          </div>
+        ) : (
+            <NavLink to="/login">Login</NavLink>
+        )}
+        {/* {currentUser && (
+          <nav>
+            <NavLink to="/create-post">Drop-a-Seed</NavLink>
+            <NavLink to="/how-it-works">How It Works</NavLink>
+            <NavLink to="/about">About</NavLink>
+          </nav>
+        )} */}
+      </header>
+      {children}
+  </div>
+    // <nav>
+    //   <NavLink className="home" to="/">
+    //     <h1>Maison des Plants</h1>
+    //   </NavLink>
+    // <div>
+    //   <div className="link-wrapper">
+    //     <div className="links">
+    //       {currentUser && (
+    //         <div className="link">
+    //           Welcome, {currentUser.email}
+    //         </div>
+    //       )}
+    //       {alwaysOptions}
+    //       {currentUser ? authenticatedOptions : unauthenticatedOptions}
+    //     </div>
+    //   </div>
+
+      /* <header>
         <Link to="/">
           <h1>Maison des Plantes</h1>
         </Link>
-      
-      <div className="auth-options">
-        {/* {currentUser ? <Link to="/">MAISON des PLANTES</Link> : null}
+        {currentUser ? (
+          <div>
+            <p>{currentUser.firstName.lastName}</p>
+            <button onClick={handleLogout}>Logout</button>
+          </div>
+        ) : (
+          <Link to='/login'></Link>
+        )} */
+
+      /* <div className="auth-options">
+        {currentUser ? <Link to="/">MAISON des PLANTES</Link> : null}
         {currentUser ? <Link to="/create-post">Drop-a-Seed</Link> : null}
         {currentUser ? <Link to="/how-it-works">How It Works</Link> : null}
-        {currentUser ? <Link to="/about">About</Link> : null} */}
-        {/* <p>
+        {currentUser ? <Link to="/about">About</Link> : null}
+        <p>
           {currentUser ? `Welcome, ${currentUser.firstName.lastName}` : null}
-        </p> */}
+        </p>
         {currentUser ? (
           <div>
             <p>{currentUser.firstName.lastName}</p>
@@ -34,9 +116,10 @@ const Nav = ({ currentUser, handleLogout, children }) => {
           </nav>
         )}
         </div>
-        {children}
-        </header>
-    </div>
+        {children} */
+      /* </header> */
+      // </div>
+      // </nav>
   );
 };
 
