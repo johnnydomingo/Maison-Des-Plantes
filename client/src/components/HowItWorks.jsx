5ef32ed10a2042218ab108882355d27a18118174
Johnny Domingo <johnnydomingo@Johnnys-MBP.lan>
2021-12-13 13:53:53 -0500

    Update

---
+++ client/src/components/HowItWorks.jsx
@@ -0,0 +1,22 @@
+const HowItWorks = () => {
+  return (
+    <div>
+      <div>
+        <h3 className="instructions">Dropping a Seed is as easy as counting to three!</h3>
+      </div>
+      <div className="card-one">
+        <h3 className="step-1">1.</h3>
+        <p className="one">Simply login and begin posting. If you're not a registered user, follow the prompt to create an account!</p>
+      </div>
+      <div className="card-two">
+        <h3 className="step-2">2.</h3>
+        <p className="two">Once you're logged in, start creating your post. Type a few lines you wish to share and add a picture of your plant!</p>
+      </div>
+      <div className="card-three">
+        <h3 className="step-three">3.</h3>
+        <p className="three">Hit the publish button to see your post go LIVE. Feel free to browse other peopl's posts.</p>
+      </div>
+    </div>
+  ) 
+};
+export default HowItWorks;
--- client/src/screens/Home.jsx
+++ client/src/screens/Home.jsx
@@ -1,33 +1,44 @@
-import Feed from '../components/Feed.jsx';
-import { Link } from 'react-router-dom';
+import Feed from "../components/Feed.jsx";
+import HowItWorks from "../components/HowItWorks.jsx";
+import { Link } from "react-router-dom";
 
 const Home = ({ posts, currentUser }) => {
-
   return (
     <div>
       <div className="sub-heading">
         <h4>A Place to Share Your House Plants...</h4>
       </div>
       <div id="share">
-        <h5>Calling all plant parents who want to show off their green children!</h5>
+        <h5>
+          Calling all plant parents who want to show off their plant babies.
+        </h5>
       </div>
       {currentUser ? (
         <div className="drop-something">
-        <Link to="/create-post">
-        <button type="button" className="home-button">Drop Somethin'</button>
-        </Link>
-      </div>
+          <Link to="/create-post">
+            <button type="button" className="home-button">
+              Drop Somethin'
+            </button>
+          </Link>
+        </div>
       ) : (
-          <Link to='/login'>Drop Somethin'</Link>
+        <Link to="/login">Drop Somethin'</Link>
       )}
       <div className="home-image">
-        <img src='https://cdn.shopify.com/s/files/1/0150/6262/products/the-sill_large-monstera_gallery_large_all_all_04_768x.jpg?v=1624309866' alt='title'/>
+        <img
+          src="https://cdn.shopify.com/s/files/1/0150/6262/products/the-sill_large-monstera_gallery_large_all_all_04_768x.jpg?v=1624309866"
+          alt="title"
+        />
       </div>
+      <br />
+      <HowItWorks />
+      <br />
       <Feed
-      posts={posts}
+        posts={posts}
       />
+      <br />
     </div>
-  )
-}
+  );
+};
 
 export default Home;
