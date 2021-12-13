13cc3bf8e58f126802f9d5e2230ba4caee495d37
Johnny Domingo <johnnydomingo@Johnnys-MBP.lan>
2021-12-13 09:43:35 -0500

    Update Home.jsx

--- client/src/screens/Home.jsx
+++ client/src/screens/Home.jsx
@@ -1,7 +1,7 @@
 import Feed from '../components/Feed.jsx';
 import { Link } from 'react-router-dom';
 
-const Home = ({ posts }) => {
+const Home = ({ posts, currentUser }) => {
 
   return (
     <div>
@@ -11,11 +11,15 @@ const Home = ({ posts }) => {
       <div id="share">
         <h5>Calling all plant parents who want to show off their green children!</h5>
       </div>
-      <div className="drop-something">
-        <Link to="/login">
+      {currentUser ? (
+        <div className="drop-something">
+        <Link to="/create-post">
         <button type="button" className="home-button">Drop Somethin'</button>
         </Link>
       </div>
+      ) : (
+          <Link to='/login'>Drop Somethin'</Link>
+      )}
       <div className="home-image">
         <img src='https://cdn.shopify.com/s/files/1/0150/6262/products/the-sill_large-monstera_gallery_large_all_all_04_768x.jpg?v=1624309866' alt='title'/>
       </div>
