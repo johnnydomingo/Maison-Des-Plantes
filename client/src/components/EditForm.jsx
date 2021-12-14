d8b7d125193513d1031bc0dcf21bfbb4e68efc30
Johnny Domingo <johnnydomingo@Johnnys-MBP.lan>
2021-12-14 18:29:05 -0500

    Update

--- client/src/components/EditForm.jsx
+++ client/src/components/EditForm.jsx
@@ -1,12 +1,11 @@
-import { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
-import '../css/EditForm.css';
-
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import "../css/EditForm.css";
 
 const EditForm = ({ posts, handlePostUpdate }) => {
   const [formData, setFormData] = useState({
-    content: '',
-    image_url: '',
+    content: "",
+    image_url: "",
   });
   const { content, image_url } = formData;
   const { id } = useParams();
@@ -14,7 +13,10 @@ const EditForm = ({ posts, handlePostUpdate }) => {
   useEffect(() => {
     const prefillFormData = () => {
       const postContent = posts.find((post) => post.id === Number(id));
-      setFormData({ content: postContent.content, image_url: postContent.image_url });
+      setFormData({
+        content: postContent.content,
+        image_url: postContent.image_url,
+      });
     };
     if (posts) prefillFormData();
   }, [posts, id]);
@@ -25,28 +27,38 @@ const EditForm = ({ posts, handlePostUpdate }) => {
       ...prevState,
       [name]: value,
     }));
-    
   };
 
   return (
     <form
+      id="edit-form"
       onSubmit={(ev) => {
         ev.preventDefault();
         handlePostUpdate(id, formData);
       }}
     >
-      <h3>Not happy with your post? Edit it!</h3>
-      <label>
-        Content:
-        <input type='text' name='content' value={content} onChange={handleChange} />
-      </label>
-      <label>
-        Image URL:
-        <input type='text' name='image_url' value={image_url} onChange={handleChange} />
-      </label>
-      <button>Publish</button>
+      <h3 id="not-happy">Not happy with your post? Edit it!</h3>
+      <div className="form-container">
+        <textarea
+          id="content-box"
+          type="text"
+          name="content"
+          placeholder="Content"
+          value={content}
+          onChange={handleChange}
+        />
+        <textarea
+          id="image-box"
+          type="text"
+          name="image_url"
+          placeholder="Image-Url"
+          value={image_url}
+          onChange={handleChange}
+        />
+        <button id="publish-button">Publish</button>
+      </div>
     </form>
   );
-}
+};
 
 export default EditForm;
