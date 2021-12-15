866f7165ecade00dfcad4b3b15a165625ca2d4e1
Johnny Domingo <johnnydomingo@Johnnys-MBP.lan>
2021-12-15 10:14:52 -0500

    Update Feed

--- client/src/components/Feed.jsx
+++ client/src/components/Feed.jsx
@@ -4,18 +4,14 @@ import '../css/Feed.css'
 
 
 
-const Feed = ({ posts, handlePostDelete, currentUser }) => {
+const Feed = ({ posts }) => {
   return (
-    <div>
+    <div className="feed">
       {posts?.map((post) => (
         <div key={post.id}>
           <Link to={`/posts/${post.id}`}>
-            {/* { post?.user &&  */}
-              
-            {/* } */}
-          {/* <p>{post.content}</p> */}
-            <img id="post-image" src={post?.image_url} alt="title" />
-            <p>{post?.user?.first_name} {post?.user?.last_name}</p>
+            <img id="feed-image" src={post?.image_url} alt="title" />
+            <p id="feed-user">{post?.user?.first_name} {post?.user?.last_name}</p>
           </Link>
         </div>
       ))}
