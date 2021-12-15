1972c27371f4e1c54eeabd735903f1584e754229
Johnny Domingo <johnnydomingo@Johnnys-MBP.lan>
2021-12-15 11:45:27 -0500

    Update

--- client/src/components/Feed.jsx
+++ client/src/components/Feed.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import '../css/Feed.css'
-
-
+import "../css/Feed.css";
 
 const Feed = ({ posts }) => {
   return (
@@ -10,11 +8,12 @@ const Feed = ({ posts }) => {
       {posts?.map((post) => (
         <div key={post.id}>
           <div className="hover">
-          <Link to={`/posts/${post.id}`}>
-            <img id="feed-image" src={post?.image_url} alt="title" />
-            <p id="feed-user">{post?.user?.first_name} {post?.user?.last_name}</p>
+            <Link to={`/posts/${post.id}`}>
+              <img id="feed-image" src={post?.image_url} alt="title" />
+              <p id="feed-user">by: {post?.user?.first_name} {post?.user?.last_name}
+              </p>
             </Link>
-            </div>
+          </div>
         </div>
       ))}
     </div>
